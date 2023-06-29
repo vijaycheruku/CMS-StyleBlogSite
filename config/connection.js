8a99083ddef508ff620a7ca7cdcbee54a0baf007
vijaycheruku <126775249+vijaycheruku@users.noreply.github.com>
2023-06-29 14:32:27 +0800

    connection reverted

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,21 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 const pg = require('pg');
 
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql',
-    });
+// const sequelize = process.env.DATABASE_URL
+//   ? new Sequelize(process.env.DATABASE_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       port: 3306,
+//       dialect: 'mysql',
+//     });
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   }
-// })
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
 
 module.exports = sequelize;
