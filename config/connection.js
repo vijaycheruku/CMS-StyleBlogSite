12f74ace9b0cb6511cbf7a5940d690942bee2b9f
vijaycheruku <126775249+vijaycheruku@users.noreply.github.com>
2023-06-29 14:30:20 +0800

    DATABASE_URL added

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 const pg = require('pg');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       port: 3306,
